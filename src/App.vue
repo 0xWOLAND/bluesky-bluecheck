b77cc505ca2a168a4ba84748d5ff55a769d527de
Bhargav Annem <bannem@caltech.edu>
2024-12-17 16:46:22 -0600

    feat: init

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts"></script>
+<template>
+  <div class="hello-world">
+    <h1>Hello World</h1>
+  </div>
+</template>
+
+<style scoped>
+.hello-world {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+h1 {
+  font-size: 2rem;
+  color: var(--color-heading);
+}
+</style>
